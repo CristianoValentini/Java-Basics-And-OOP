1e63f11ae2ee8147550e81025476d0d023666ce2
Cristiano <cristianovalentini222@gmail.com>
2023-03-24 18:55:44 +0100

    adding files

---
+++ src/javaoop/_1/Course.java
@@ -0,0 +1,15 @@
+package javaoop._1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course {
+    String courseName = "" ;
+    Integer maxStudents = 0 ;
+    Byte qualityRatingOutOf10 = 0 ;
+
+    String namesOfTeacher = "" ;
+    int femaleStudents = 0 ;
+
+    List<Student> students = new ArrayList<>();
+}
---
+++ src/javaoop/_1/Student.java
@@ -0,0 +1,6 @@
+package javaoop._1;
+
+public class Student {
+    String name ;
+    int age ;
+}
